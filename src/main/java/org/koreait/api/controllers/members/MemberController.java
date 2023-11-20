d6207749e351de7828c6456b4013d9a9616e616d
yonggyo1125 <yonggyo00@kakao.com>
2023-11-20 12:35:03 +0900

    수업 소스

---
+++ src/main/java/org/koreait/api/controllers/members/MemberController.java
@@ -0,0 +1,15 @@
+package org.koreait.api.controllers.members;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/member")
+public class MemberController {
+
+    @PostMapping
+    public void join() {
+
+    }
+}
