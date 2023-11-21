36735902e8a64ef09fd3ab17f70bec4ecf7fd02a
yonggyo1125 <yonggyo00@kakao.com>
2023-11-21 12:09:02 +0900

    수업 소스

---
+++ src/main/java/org/koreait/configs/jwt/JwtConfig.java
@@ -0,0 +1,16 @@
+package org.koreait.configs.jwt;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(JwtProperties.class)
+public class JwtConfig {
+
+    @Bean
+    public TokenProvider tokenProvider(JwtProperties jwtProperties) {
+        System.out.println("jwtProperties:" + jwtProperties);
+        return new TokenProvider(jwtProperties.getSecret(), jwtProperties.getAccessTokenValidityInSeconds());
+    }
+}
--- src/main/java/org/koreait/configs/jwt/TokenProvider.java
+++ src/main/java/org/koreait/configs/jwt/TokenProvider.java
@@ -1,29 +1,21 @@
 package org.koreait.configs.jwt;
 
-import io.jsonwebtoken.io.Decoder;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
-@Component
-@EnableConfigurationProperties(JwtProperties.class)
 public class TokenProvider {
-    @Autowired
-    private JwtProperties jwtProperties;
 
     private final String secret;
-    private final long tokeValidityInSeconds;
+    private final long tokenValidityInSeconds;
 
     private Key key;
 
-    public TokenProvider() {
-        secret = jwtProperties.getSecret();
-        tokeValidityInSeconds = jwtProperties.getAccessTokenValidityInSeconds();
+    public TokenProvider(String secret, Long tokenValidityInSeconds) {
+        this.secret = secret;
+        this.tokenValidityInSeconds = tokenValidityInSeconds;
 
         byte[] bytes = Decoders.BASE64.decode(secret);
         key = Keys.hmacShaKeyFor(bytes);
