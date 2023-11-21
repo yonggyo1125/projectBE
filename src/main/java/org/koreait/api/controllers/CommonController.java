76b74acab5313a93ac8b74ab757a3a8bc4844564
yonggyo1125 <yonggyo00@kakao.com>
2023-11-21 10:17:55 +0900

    수업 소스

--- src/main/java/org/koreait/api/controllers/CommonController.java
+++ src/main/java/org/koreait/api/controllers/CommonController.java
@@ -13,16 +13,19 @@ public class CommonController {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<JSONData> errorHandler(Exception e) {
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        Object message = e.getMessage();
 
         if (e instanceof CommonException) {
             CommonException commonException = (CommonException) e;
             status = commonException.getStatus();
+
+            if (commonException.getMessages() != null) message = commonException.getMessages();
         }
 
         JSONData data = new JSONData();
         data.setSuccess(false);
         data.setStatus(status);
-        data.setMessage(e.getMessage());
+        data.setMessage(message);
 
         e.printStackTrace();
 
