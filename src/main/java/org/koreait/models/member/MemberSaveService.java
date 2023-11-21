39834866cb549db725f2b4cd32a1e6be8c5af80f
yonggyo1125 <yonggyo00@kakao.com>
2023-11-21 10:49:08 +0900

    수업 소스

---
+++ src/main/java/org/koreait/models/member/MemberSaveService.java
@@ -0,0 +1,47 @@
+package org.koreait.models.member;
+
+import lombok.RequiredArgsConstructor;
+import org.koreait.api.controllers.members.JoinValidator;
+import org.koreait.api.controllers.members.RequestJoin;
+import org.koreait.entities.Member;
+import org.koreait.repositories.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
+
+@Service
+@RequiredArgsConstructor
+public class MemberSaveService {
+    private final MemberRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JoinValidator joinValidator;
+
+    public void save(RequestJoin form, Errors errors) {
+        joinValidator.validate(form, errors);
+
+        if (errors.hasErrors()) {
+            return;
+        }
+
+        // 회원 가입 처리
+        String hash = passwordEncoder.encode(form.password());
+        Member member = Member.builder()
+                .email(form.email())
+                .name(form.name())
+                .password(hash)
+                .mobile(form.mobile())
+                .build();
+
+        save(member);
+    }
+
+    public void save(Member member) {
+        String mobile = member.getMobile();
+        if (member != null) {
+            mobile = mobile.replaceAll("\\D", "");
+            member.setMobile(mobile);
+        }
+
+        repository.saveAndFlush(member);
+    }
+}
