628c0923d1beb77163302852abeca914b8f5362c
yonggyo1125 <yonggyo00@kakao.com>
2023-11-22 11:18:09 +0900

    수업 소스

--- src/main/java/org/koreait/configs/SecurityConfig.java
+++ src/main/java/org/koreait/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.koreait.configs;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.koreait.configs.jwt.CustomJwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -7,9 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -25,6 +28,29 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(c -> c.disable())
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(customJwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.exceptionHandling(c -> {
+            c.authenticationEntryPoint((req,  res,  e) -> {
+                res.sendError(HttpServletResponse.SC_UNAUTHORIZED); //  401
+            });
+
+            c.accessDeniedHandler((req,res, e) -> {
+                res.sendError(HttpServletResponse.SC_FORBIDDEN); // 403
+            });
+        });
+
+        http.authorizeHttpRequests(c -> {
+            c.requestMatchers(
+                    "/api/v1/member", // 회원가입
+                    "/api/v1/member/token",  // 로그인
+                    "/api/v1/member/exists/**").permitAll()
+                    .anyRequest().authenticated(); // 나머지 URL은 모두 회원 인증(토큰 인증)
+        });
+
 
         return http.build();
     }
